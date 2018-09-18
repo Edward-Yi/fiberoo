d6276ea57391b78fee2a5b4a92742fc09153c060
Edward Yi <42788217+Edward-Yi@users.noreply.github.com>
2018-09-18 12:36:36 -1000

    Code

---
+++ fiberoo.js
@@ -0,0 +1,13 @@
+function fib() {
+  const list = [0, 1];
+  for (let i = 1; i < 99; i++) {
+    list[i + 1] = list[i] + list[i - 1];
+  }
+  return list;
+}
+console.log(fib());
+
+function numToString(nums) {
+  return _.map(nums, function (num) { return `${num}`; });
+}
+console.log(numToString(fib()));
